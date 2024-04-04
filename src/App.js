097d933ef572bc56ec4293a812ab5de24614c728
yigitertor <61631906+yigitertor@users.noreply.github.com>
2024-04-04 23:43:50 +0300

    City Component UI Update

--- src/App.js
+++ src/App.js
@@ -5,20 +5,20 @@ import CityComponent from "./modules/CityComponent";
 import WeatherComponent from "./modules/WeatherInfoComponent";
 
 export const WeatherIcons = {
-  "01d": "/react-weather-app/icons/sunny.svg",
-  "01n": "/react-weather-app/icons/night.svg",
-  "02d": "/react-weather-app/icons/day.svg",
-  "02n": "/react-weather-app/icons/cloudy-night.svg",
-  "03d": "/react-weather-app/icons/cloudy.svg",
-  "03n": "/react-weather-app/icons/cloudy.svg",
-  "04d": "/react-weather-app/icons/perfect-day.svg",
-  "04n": "/react-weather-app/icons/cloudy-night.svg",
-  "09d": "/react-weather-app/icons/rain.svg",
-  "09n": "/react-weather-app/icons/rain-night.svg",
-  "10d": "/react-weather-app/icons/rain.svg",
-  "10n": "/react-weather-app/icons/rain-night.svg",
-  "11d": "/react-weather-app/icons/storm.svg",
-  "11n": "/react-weather-app/icons/storm.svg",
+  "01d": "/icons/sunny.svg",
+  "01n": "/icons/night.svg",
+  "02d": "/icons/day.svg",
+  "02n": "/icons/cloudy-night.svg",
+  "03d": "/icons/cloudy.svg",
+  "03n": "/icons/cloudy.svg",
+  "04d": "/icons/perfect-day.svg",
+  "04n": "/icons/cloudy-night.svg",
+  "09d": "/icons/rain.svg",
+  "09n": "/icons/rain-night.svg",
+  "10d": "/icons/rain.svg",
+  "10n": "/icons/rain-night.svg",
+  "11d": "/icons/storm.svg",
+  "11n": "/icons/storm.svg",
 };
 
 const Container = styled.div`
@@ -26,20 +26,14 @@ const Container = styled.div`
   flex-direction: column;
   align-items: center;
   width: 380px;
+  height: 820px;
   padding: 20px 10px;
   margin: auto;
   border-radius: 4px;
   box-shadow: 0 3px 6px 0 #555;
-  background: white;
-  font-family: Montserrat;
+  background: #13131a;
 `;
 
-const AppLabel = styled.span`
-  color: black;
-  margin: 20px auto;
-  font-size: 18px;
-  font-weight: bold;
-`;
 const CloseButton = styled.span`
   padding: 2px 3px;
   background-color: black;
@@ -54,13 +48,12 @@ function App() {
   const fetchWeather = async (e) => {
     e.preventDefault();
     const response = await Axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=fe4feefa8543e06d4f3c66d92c61b69c`
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8297725875f39e37ed6a7bc2e1fc738c`
     );
     updateWeather(response.data);
   };
   return (
     <Container>
-      <AppLabel>React Weather App</AppLabel>
       {city && weather ? (
         <WeatherComponent weather={weather} city={city} />
       ) : (
--- src/modules/CityComponent.js
+++ src/modules/CityComponent.js
@@ -6,50 +6,86 @@ const SearchBox = styled.form`
   flex-direction: row;
   justify-content: space-evenly;
   margin: 20px;
-  border: black solid 1px;
-  border-radius: 2px;
 
   & input {
+    background: #1e1e29;
+    border-radius: 8px;
     padding: 10px;
     font-size: 14px;
     border: none;
     outline: none;
-    font-family: Montserrat;
     font-weight: bold;
+    color: white;
   }
   & button {
-    background-color: black;
+    background-color: #7692c9;
     font-size: 14px;
     padding: 0 10px;
     color: white;
     border: none;
     outline: none;
     cursor: pointer;
-    font-family: Montserrat;
     font-weight: bold;
+    border-radius: 8px;
+    margin-left: 2px;
   }
 `;
 const ChooseCityLabel = styled.span`
-  color: black;
-  margin: 10px auto;
-  font-size: 18px;
-  font-weight: bold;
+  width: 311px;
+  height: 20px;
+
+  /* Text sm */
+  font-family: "Nunito";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 140%;
+
+  /* identical to box height, or 20px */
+  align-items: center;
+  text-align: center;
+
+  /* Base/Gray 200 */
+  color: #bfbfd4;
 `;
-const WelcomeWeatherLogo = styled.img`
-  width: 140px;
-  height: 140px;
+
+const WelcomeLabel = styled.div`
+  color: white;
+  font-family: "Nunito";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 140%;
+  & span {
+    color: #7692c9;
+  }
+`;
+
+const AppLogo = styled.img`
+  width: 186px;
+  height: 32px;
+  left: calc(50% - 186px / 2 - 0.5px);
+  top: 48px;
+  opacity: 0.8;
   margin: 40px auto;
+  margin-bottom: 140px;
 `;
+
 const CityComponent = (props) => {
   const { updateCity, fetchWeather } = props;
   return (
     <>
-      <WelcomeWeatherLogo src={"/react-weather-app/icons/perfect-day.svg"} />
-      <ChooseCityLabel>Find Weather of your city</ChooseCityLabel>
+      <AppLogo src={"/icons/logo.png"} />
+      <WelcomeLabel>
+        Welcome to <span>TypeWeather</span>
+      </WelcomeLabel>
+      <ChooseCityLabel>
+        Choose a location to see the weather forecast
+      </ChooseCityLabel>
       <SearchBox onSubmit={fetchWeather}>
         <input
           onChange={(e) => updateCity(e.target.value)}
-          placeholder="City"
+          placeholder="Search location"
         />
         <button type={"submit"}>Search</button>
       </SearchBox>
