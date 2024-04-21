f79b1cbe07d1b5c67f75e849aa024f1f106e8640
yigitertor <yigitertor@hotmail.com>
2024-04-21 21:54:47 +0300

    icon fix

--- src/modules/WeatherInfoComponent.js
+++ src/modules/WeatherInfoComponent.js
@@ -6,8 +6,8 @@ import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import ForecastContainer from "./ForecastContainer";
 
 const WeatherInfoIcons = {
-  Sunset: "/icons/temp.svg",
-  Sunrise: "/icons/temp.svg",
+  Sunset: "/icons/sunset.svg",
+  Sunrise: "/icons/sunrise.svg",
   Humidity: "/icons/humidity.svg",
   Wind: "/icons/wind.svg",
   Pressure: "/icons/pressure.svg",
